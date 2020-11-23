1c98fb767078ea7c01c6ced6734d79fb8dd878d2
Cybsac <30456774+Machinesaac@users.noreply.github.com>
2020-11-23 15:45:32 +0800

    获取数据库数据

--- pages/taskbar/taskbar.js
+++ pages/taskbar/taskbar.js
@@ -3,10 +3,15 @@ const jobs = db.collection('jobs')
 
 Page({
   data:{
-    set: db.collection('todos').get({
+
+  },
+  onLoad: function(){
+    var that = this
+    jobs.get({
       success: function(res) {
         // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
         console.log(res.data)
+        that.setData({s:res.data})
       }
     })
   }
