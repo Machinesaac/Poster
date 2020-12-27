d1f47544967bbf1bc6f2834e5df35c424045b865
Machinesaac <779568624@qq.com>
2020-12-27 14:02:53 +0800

    初步完成悬赏栏框架

--- pages/expdetail/expdetail.js
+++ pages/expdetail/expdetail.js
@@ -1,11 +1,24 @@
+const db = wx.cloud.database({
+  env:"poster-7gmqnwnb215c1afc"
+})
+const jobs = db.collection("jobs")
+
 Page({
   data: {
 
   },
   onLoad: function (options) {
     var id = options.id
+    var that = this
     this.setData({
-      id: id
+      id: id,
+      
+    })
+    jobs.doc(id).get({
+      success: function(res) {
+        console.log(res.data)
+        that.setData({s: res.data})
+      }
     })
   }
 })
